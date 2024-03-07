4c672bacb7b7f94f08a3c57fad32c7b90c67bdbb
duythinh <thinhogg11@gmail.com>
2024-03-07 08:31:10 +0700

    update

--- app/quotes/[quotesId]/page.tsx
+++ app/quotes/[quotesId]/page.tsx
@@ -7,7 +7,7 @@ interface paramsI {
 export default function QuotesPage({ params }: paramsI) {
   return (
     <>
-      <h1>Quotes detail page</h1>
+      <h1>Quotes detail pages</h1>
       <QuotesDetail id={params.quotesId} />
     </>
   );
